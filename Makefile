715b169b94244b9d79453275d929d90612f61bc1
Aleem Isiaka <aleemisiaka@gmail.com>
2022-04-21 12:35:29 +0100

    Update

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 TEST?=$$(go list ./... | grep -v 'vendor')
-HOSTNAME=onboardbase.com
-NAMESPACE=providers
+HOSTNAME=registry.terraform.io
+NAMESPACE=hashicorp
 NAME=onboardbase
 BINARY=terraform-provider-${NAME}
 VERSION=0.0.1
--- examples/main.tf
+++ examples/main.tf
@@ -8,12 +8,12 @@ terraform {
 }
 # Initialize the provider
 provider "onboardbase" {
-  apikey = var.onboardbase_apikey // Your onboardbase API key
+  apikey   = var.onboardbase_apikey   // Your onboardbase API key
   passcode = var.onboardbase_passcode // Your passcode
 }
 # Fetch all the backend environment secrets
 resource "onboardbase_resource" "backend_env" {
-  project = "backend-env"
+  project     = "backend-env"
   environment = "development"
   // Specify the keys to return
   keys = ["PROJECTS_KEY", "SENTRY_SERVER_DSN"]
@@ -21,12 +21,12 @@ resource "onboardbase_resource" "backend_env" {
 
 // Setup api key as a string
 variable "onboardbase_apikey" {
-  default = "T6T8UW6R36XWXJNHACDEHJ"
+  default     = "T6T8UW6R36XWXJNHACDEHJ"
   description = "An API key to authenticate with Onboardbase"
 }
 // Make the passcode an input
 variable "onboardbase_passcode" {
-  type = string
+  type        = string
   description = "The passcode for the API key"
 }
 # Output all the selected keys
